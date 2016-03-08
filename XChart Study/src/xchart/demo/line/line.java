bab78a9d75d35e7fc9b1915b44be6d40b5a1adc3
kankankk <kankankkkkkk@sina.com>
2016-03-08 23:26:16 +0800

    Signed-off-by: kankankk <kankankkkkkk@sina.com>

---
+++ XChart Study/src/xchart/demo/line/line.java
@@ -0,0 +1,26 @@
+package xchart.demo.line;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class line {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		//显示JFrame
+		Chart chart = new line().getChart();
+		new SwingWrapper(chart).displayChart(); 
+
+
+	}
+
+	private Chart getChart() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	
+}
